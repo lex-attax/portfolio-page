a68d00a21e503fec7e20e8c9939e3bc417d5f6df
Alex Pavlovic <lex.pavlovic@gmail.com>
2018-05-14 05:22:36 +0000

    add contact route

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { AboutComponent } from '../about/about.component';
+import { ContactComponent } from '../contact/contact.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
   { path: 'portfolio', component: PortfolioComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   ];
 
 @NgModule({
